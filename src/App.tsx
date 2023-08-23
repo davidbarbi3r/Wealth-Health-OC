c90a2dd7426d7ace61746c8c24043d44330a4d20
davidbarbi3r <davidbarbi3r@gmail.com>
2023-08-23 14:43:04 +0200

    style(date-picker): removed locales add responsive
    BREAKING CHANGE: add responsive and modal behavior

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef} from "react";
 import "./App.css";
 
 interface AppProps {
-  locale?: "en" | "fr" | "es";
   defaultDate?: string;
   wrapperHeight?: string;
   wrapperWidth?: string;
@@ -10,10 +9,9 @@ interface AppProps {
 }
 
 function App({
-  locale = "es",
   defaultDate,
-  wrapperHeight = "400px",
-  wrapperWidth = "650px",
+  wrapperHeight = "auto",
+  wrapperWidth = "auto",
   showColumnIndex = true,
 }: AppProps) {
   const [date, setDate] = useState(
@@ -28,11 +26,7 @@ function App({
   const decades = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const years = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const longMonths = ["01", "03", "05", "07", "08", "10", "12"];
-  const columnIndex = {
-    fr: ["Année", "Mois", "Jour"],
-    en: ["Year", "Month", "Day"],
-    es: ["Año", "Mes", "Día"],
-  };
+  const columnIndex = ["Year", "Month", "Day"]
   const dozeThirtyButton = useRef<HTMLButtonElement>(null);
   const dayNineButton = useRef<HTMLButtonElement>(null);
   const dayZeroButton = useRef<HTMLButtonElement>(null);
@@ -40,24 +34,17 @@ function App({
   const daysButtons: HTMLButtonElement[] = Array.from(
     document.querySelectorAll('[name="day-unit"]')
   );
-  const [stepper, setStepper] = useState<number>(1)
-
+  const [currentView, setCurrentView] = useState<'YEAR' | 'MONTH' | 'DAY'>('YEAR');
   useEffect(() => {
     const datePickerWrapper: HTMLDivElement = document.querySelector(
       ".date-selector__wrapper"
     ) as HTMLDivElement;
-    const yearWrapper: HTMLDivElement = document.querySelector(
-      ".date-selector__wrapper__year"
-    ) as HTMLDivElement;
     if (datePickerWrapper.style) {
-      locale !== "en" && (datePickerWrapper.style.gridTemplateAreas = `"day-title month-title year-title" "day month year"`, datePickerWrapper.style.gridTemplateColumns = "30% 20% 50%")
+      (datePickerWrapper.style.gridTemplateAreas = `"year-title month-title day-title" "year month day"`, datePickerWrapper.style.gridTemplateColumns = "50% 20% 30%")
       datePickerWrapper.style.height = wrapperHeight;
       datePickerWrapper.style.width = wrapperWidth;
     }
-    if (yearWrapper.style) {
-      console.log("test");
-    }
-  }, [wrapperHeight, wrapperWidth, locale]);
+  }, [wrapperHeight, wrapperWidth]);
 
   const isYearBisextile = (year: number) => {
     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
@@ -67,54 +54,23 @@ function App({
     const year = date.slice(0, 4);
     const month = date.slice(5, 7);
     const day = date.slice(8, 10);
-
-    switch (locale) {
-      case "en":
-        if (isInput){
-          return `${month}-${parseInt(day) < 10 ? '0'+day : day}-${year}`
-        }
-        return `${months[locale][parseInt(month) - 1]} ${day}, ${year}`;
-      case "fr":
-        if (isInput){
-          return `${parseInt(day) < 10 ? '0'+day : day}-${month}-${year}`
-        }
-        return `${day} ${months[locale][parseInt(month) - 1]} ${year}`;
-      case "es":
-        if (isInput){
-          return `${parseInt(day) < 10 ? '0'+day : day}-${month}-${year}`
-        }
-        return `${day} de ${months[locale][parseInt(month) - 1]} de ${year}`
-      default:
-        if (isInput){
-          return `${month}-${parseInt(day) < 10 ? '0'+day : day}-${year}`
-        }
-        return `${months[locale][parseInt(month) - 1]} ${day}, ${year}`;
+    if (isInput){
+      return `${month}-${parseInt(day) < 10 ? '0'+day : day}-${year}`
     }
+    return `${months[parseInt(month) - 1]} ${day}, ${year}`;
   };
 
   const nextStep = () => {
-    setStepper((step) => {
-      if (step = 3){
-        return 3
-      }
-      return step + 1
-    })
-    console.log(stepper)
+    if (currentView === 'YEAR') setCurrentView('MONTH');
+    else if (currentView === 'MONTH') setCurrentView('DAY');
   }
 
   const prevStep = () => {
-    setStepper((step) => {
-          if (step = 1) {
-            return 1
-          }
-          return step - 1;
-        }
-    )
-    console.log(stepper)
+    if (currentView === 'MONTH') setCurrentView('YEAR');
+    else if (currentView === 'DAY') setCurrentView('MONTH');
   }
 
-  const months = {
-    en: [
+  const months = [
       "January",
       "February",
       "March",
@@ -127,50 +83,15 @@ function App({
       "October",
       "November",
       "December",
-    ],
-    fr: [
-      "Janvier",
-      "Février",
-      "Mars",
-      "Avril",
-      "Mai",
-      "Juin",
-      "Juillet",
-      "Août",
-      "Septembre",
-      "Octobre",
-      "Novembre",
-      "Décembre",
-    ],
-    es: [
-      "Enero",
-      "Febrero",
-      "Marzo",
-      "Abril",
-      "Mayo",
-      "Junio",
-      "Julio",
-      "Agosto",
-      "Septiembre",
-      "Octobre",
-      "Noviembre",
-      "Diciembre",
-    ],
-  };
+    ];
 
   const doze = ["0", "1", "2", "3"];
   const days = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newDate = e.currentTarget.value;
-    console.log(locale)
-    let dateRegex;
-    switch (locale) {
-      case "en":
-        dateRegex = /^(19|20|21)[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
-      default:
-        dateRegex = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19|20|21)[0-9]{2}$/
-    }
+    console.log()
+    const dateRegex = /^(19|20|21)[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
     if (!dateRegex.test(newDate)) {
       setErrorMessage('Please enter a valid date in YYYY-MM-DD format.');
@@ -275,12 +196,12 @@ function App({
       case "month":
         updatedDate =
           date.slice(0, 5) +
-          (months[locale].indexOf(e.currentTarget.textContent as string) + 1 <
+          (months.indexOf(e.currentTarget.textContent as string) + 1 <
           10
             ? "0" +
-              (months[locale].indexOf(e.currentTarget.textContent as string) +
+              (months.indexOf(e.currentTarget.textContent as string) +
                 1)
-            : months[locale].indexOf(e.currentTarget.textContent as string) + 1
+            : months.indexOf(e.currentTarget.textContent as string) + 1
           ).toString() +
           date.slice(7, 10);
         setDate(updatedDate);
@@ -312,12 +233,6 @@ function App({
     handleDayZeroAndDozenZero(updatedDate)
   };
 
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    return (
-        <p>mobile</p>
-    )
-  }
-
   return (
     <>
       <form>
@@ -326,130 +241,134 @@ function App({
           type="text"
           id="date"
           name="date"
-          value={formatDate(date, true)}
-          onChange={(e) => handleDateChange(e)}
+          value={date}
+          onChange={(e) => {
+            handleDateChange(e);
+          }}
           pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
         />
         {errorMessage && <p>{errorMessage}</p>}
         <div onClick={() => toggleDateSelector()}>{">"}</div>
-        <div className={`date-selector ${isDateSelectorOpen ? "open" : ""}`}>
-          <h2>Choose a date</h2>
-          <div className="date-selector__wrapper">
-            <div className="date-selector__mobile-nav">
-              <span className="date-selector__mobile-nav-left" onClick={prevStep}>gauche</span>
-              <span className="date-selector__mobile-nav-right" onClick={nextStep}>droite</span>
-            </div>
-            {showColumnIndex ? (
-              <>
-                <div className="date-selector__wrapper__year__column-index">
-                  {columnIndex[locale][0]}
-                </div>
-                <div className="date-selector__wrapper__month__column-index">
-                  {columnIndex[locale][1]}
-                </div>
-                <div className="date-selector__wrapper__day__column-index">
-                  {columnIndex[locale][2]}
-                </div>
-              </>
-            ) : (
-              ""
-            )}
-            <div className="date-selector__wrapper__year">
-              <div className="date-selector__wrapper__year__century">
-                {centuries.map((century) => (
-                  <button
-                    type="button"
-                    name="year-century"
-                    key={"cent" + century}
-                    onClick={(e) => dateSelector(e)}
-                    className={century === date.slice(0, 2) ? "active" : ""}
-                  >
-                    {century}
-                  </button>
-                ))}
+        <div className={`modal ${isDateSelectorOpen ? "open" : ""}`}>
+          <div className={`modal-content ${isDateSelectorOpen ? "open" : ""}`}>
+            <h2>Choose a date</h2>
+            <div className="date-selector__wrapper">
+              <div className="date-selector__mobile-nav">
+                <span className="date-selector__mobile-nav-left" onClick={prevStep}>gauche</span>
+                <span className="date-selector__mobile-nav-right" onClick={nextStep}>droite</span>
               </div>
-              <div className="date-selector__wrapper__year__decade">
-                {decades.map((decade) => (
+              {showColumnIndex ? (
+                <>
+                  <div className={`date-selector__wrapper__year__column-index ${currentView !== 'YEAR' ? 'hidden' : ''}`}>
+                    {columnIndex[0]}
+                  </div>
+                  <div className={`date-selector__wrapper__month__column-index ${currentView !== 'MONTH' ? 'hidden' : ''}`}>
+                    {columnIndex[1]}
+                  </div>
+                  <div className={`date-selector__wrapper__day__column-index ${currentView !== 'DAY' ? 'hidden' : ''}`}>
+                    {columnIndex[2]}
+                  </div>
+                </>
+              ) : (
+                ""
+              )}
+              <div className={`date-selector__wrapper__year ${currentView !== 'YEAR' ? 'hidden' : ''}`}>
+                <div className="date-selector__wrapper__year__century">
+                  {centuries.map((century) => (
+                    <button
+                      type="button"
+                      name="year-century"
+                      key={"cent" + century}
+                      onClick={(e) => dateSelector(e)}
+                      className={century === date.slice(0, 2) ? "active" : ""}
+                    >
+                      {century}
+                    </button>
+                  ))}
+                </div>
+                <div className="date-selector__wrapper__year__decade">
+                  {decades.map((decade) => (
+                    <button
+                      type="button"
+                      name="year-decade"
+                      key={"dec" + decade}
+                      onClick={(e) => dateSelector(e)}
+                      className={
+                        decade.slice(0, 1) === date.slice(2, 3) ? "active" : ""
+                      }
+                    >
+                      {decade}
+                    </button>
+                  ))}
+                </div>
+                <div className="date-selector__wrapper__year__year">
+                  {years.map((year) => (
+                    <button
+                      type="button"
+                      name="year-unit"
+                      key={"year" + year}
+                      onClick={(e) => dateSelector(e)}
+                      className={year === date.slice(3, 4) ? "active" : ""}
+                    >
+                      {year}
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div className={`date-selector__wrapper__month ${currentView !== 'MONTH' ? 'hidden' : ''}`}>
+                {months.map((month) => (
                   <button
                     type="button"
-                    name="year-decade"
-                    key={"dec" + decade}
+                    name="month"
+                    key={"month" + month}
                     onClick={(e) => dateSelector(e)}
                     className={
-                      decade.slice(0, 1) === date.slice(2, 3) ? "active" : ""
+                      month === months[parseInt(date.slice(5, 7)) - 1]
+                        ? "active"
+                        : ""
                     }
                   >
-                    {decade}
+                    {month}
                   </button>
                 ))}
               </div>
-              <div className="date-selector__wrapper__year__year">
-                {years.map((year) => (
-                  <button
-                    type="button"
-                    name="year-unit"
-                    key={"year" + year}
-                    onClick={(e) => dateSelector(e)}
-                    className={year === date.slice(3, 4) ? "active" : ""}
-                  >
-                    {year}
-                  </button>
-                ))}
-              </div>
-            </div>
-            <div className="date-selector__wrapper__month">
-              {months[locale].map((month) => (
-                <button
-                  type="button"
-                  name="month"
-                  key={"month" + month}
-                  onClick={(e) => dateSelector(e)}
-                  className={
-                    month === months[locale][parseInt(date.slice(5, 7)) - 1]
-                      ? "active"
-                      : ""
-                  }
-                >
-                  {month}
-                </button>
-              ))}
-            </div>
-            <div className="date-selector__wrapper__day">
-              <div className="date-selector__wrapper__day__doze">
-                {doze.map((doz) => (
-                  <button
-                    type="button"
-                    name="day-doze"
-                    key={"doz" + doz}
-                    onClick={(e) => dateSelector(e)}
-                    className={
-                      doz.slice(0, 1) === date.slice(8, 9) ? "active" : ""
-                    }
-                    ref={doz === "3" ? dozeThirtyButton : null}
-                  >
-                    {doz}
-                  </button>
-                ))}
-              </div>
-              <div className="date-selector__wrapper__day__day">
-                {days.map((day) => (
-                  <button
-                    type="button"
-                    name="day-unit"
-                    key={"day" + day}
-                    onClick={(e) => dateSelector(e)}
-                    className={day === date.slice(9, 10) ? "active" : ""}
-                    ref={day === "9"
-                        ? dayNineButton
-                        : day === "1"
-                        ? dayOneButton
-                        : day === "0"
-                        ? dayZeroButton
-                        : null}
-                  >
-                    {day}
-                  </button>
-                ))}
+              <div className={`date-selector__wrapper__day ${currentView !== 'DAY' ? 'hidden' : ''}`}>
+                <div className="date-selector__wrapper__day__doze">
+                  {doze.map((doz) => (
+                    <button
+                      type="button"
+                      name="day-doze"
+                      key={"doz" + doz}
+                      onClick={(e) => dateSelector(e)}
+                      className={
+                        doz.slice(0, 1) === date.slice(8, 9) ? "active" : ""
+                      }
+                      ref={doz === "3" ? dozeThirtyButton : null}
+                    >
+                      {doz}
+                    </button>
+                  ))}
+                </div>
+                <div className="date-selector__wrapper__day__day">
+                  {days.map((day) => (
+                    <button
+                      type="button"
+                      name="day-unit"
+                      key={"day" + day}
+                      onClick={(e) => dateSelector(e)}
+                      className={day === date.slice(9, 10) ? "active" : ""}
+                      ref={day === "9"
+                          ? dayNineButton
+                          : day === "1"
+                          ? dayOneButton
+                          : day === "0"
+                          ? dayZeroButton
+                          : null}
+                    >
+                      {day}
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
