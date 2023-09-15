f0a83a4539664fd6aa959fb7606405dc17c0688c
davidbarbi3r <davidbarbi3r@gmail.com>
2023-09-15 17:50:32 +0200

    fix(date-picker): update package

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+    build: {
+        lib: {
+            entry: 'src/main.tsx',
+            name: 'GnarlyDatePicker',
+            fileName: (format) => `gnarly-date-picker.${format}.js`
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+        minify: false
+    }
 })
+
