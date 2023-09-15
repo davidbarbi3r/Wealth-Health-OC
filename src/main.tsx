916458a823a951ebb279c76c085e6906ab4ac33b
davidbarbi3r <davidbarbi3r@gmail.com>
2023-09-15 18:45:46 +0200

    fix(date-picker): last last try

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import GnarlyDatePicker from './GnarlyDatePicker.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const App = () => {
+    return (
         <React.StrictMode>
-            <GnarlyDatePicker/>
-        </React.StrictMode>,
-    )
+            <GnarlyDatePicker />
+        </React.StrictMode>
+    );
+};
 
+export {App, GnarlyDatePicker}; // Export the default component
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
 
